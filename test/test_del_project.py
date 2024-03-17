37694a0b16588a18a60e691cc0b0ae7b1539fc5a
4epenax <a4epenax@ya.ru>
2024-03-17 15:07:44 +0300

    доработан тест удаления проекта: теперь списки загружаются по протоколу soap

--- test/test_del_project.py
+++ test/test_del_project.py
@@ -5,13 +5,14 @@ import random
 def test_add_project(app, db):
     if len(db.get_project_list()) == 0:
         app.project.create(Project(name="Temp"))
-    old_projects = db.get_project_list()
+    old_projects = app.soap.get_project_list()
     project = random.choice(old_projects)
     app.project.open_manage_page()
     app.project.open_project_page()
     app.project.open_random_project(project)
     app.project.delete_project()
     app.project.delete_project()
-    new_projects = db.get_project_list()
+    new_projects = app.soap.get_project_list()
     old_projects.remove(project)
-    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects,
+                                                                 key=Project.id_or_max)
